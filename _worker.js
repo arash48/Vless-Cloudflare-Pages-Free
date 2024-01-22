61a366c71151289e927627c55bca277ab2c16c2f
arash48 <63741552+arash48@users.noreply.github.com>
2024-01-22 18:19:19 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '298fdd7a-d208-4521-8301-d60d9032f307';
+let userID = 'ce08b8ec-c763-4783-a2b4-aba1c75fe63b';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
